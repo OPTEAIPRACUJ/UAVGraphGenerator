c0db20769c5da09aa590fd97f6b945cd89590b6e
Maciek <maciek0057@gmail.com>
2024-06-23 16:55:36 +0200

    zmiana koloru nowego baseName na niebieski po usunieciu starego

--- js/classes/map-manager.js
+++ js/classes/map-manager.js
@@ -224,6 +224,16 @@ export default class MapManager {
       // let baseMarker = this.markers[0];
       this.markers[i].marker.bindPopup(this.markers[i].name).openPopup();
     }
+    const blueIcon = new L.Icon({
+      iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-blue.png',
+      shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+      iconSize: [25, 41],
+      iconAnchor: [12, 41],
+      popupAnchor: [1, -34],
+      shadowSize: [41, 41]
+  });
+  
+  this.markers[0].marker.setIcon(blueIcon);
   }
 
   updateConnections() {
